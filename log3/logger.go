c7aae03bfb5063858f6f4fe2eab36fd22ed0fe1d
Denis Mishankov <mishankov@mail.com>
2026-02-16 07:24:33 +0300

    Update logger even more

--- log3/logger.go
+++ log3/logger.go
@@ -4,26 +4,27 @@ import (
 	"context"
 	"io"
 	"log/slog"
+
+	"github.com/platforma-dev/platforma/log"
 )
 
 type Logger struct {
 	w       io.Writer
-	sampler func(*Event) bool
+	sampler Sampler
 	logger  *slog.Logger
+}
+
+func NewWideEventLogger(w io.Writer, s Sampler, loggerType string, contextKeys map[string]any) *Logger {
+	return &Logger{
+		sampler: s,
+		logger:  log.New(w, loggerType, slog.LevelDebug, contextKeys),
+	}
 }
 
 func (l *Logger) WriteEvent(ctx context.Context, e *Event) {
 	e.Finish()
 
-	if l.sampler(e) {
+	if l.sampler.ShouldSample(ctx, e) {
 		l.logger.LogAttrs(ctx, e.level, "", e.ToAttrs()...)
 	}
-}
-
-func mapToAttrs(data map[string]any) []slog.Attr {
-	attrs := make([]slog.Attr, 0, len(data))
-	for k, v := range data {
-		attrs = append(attrs, slog.Any(k, v))
-	}
-	return attrs
 }
---
+++ log3/sampler.go
@@ -0,0 +1,57 @@
+package log3
+
+import (
+	"context"
+	"math/rand/v2"
+	"time"
+)
+
+type Sampler interface {
+	ShouldSample(ctx context.Context, e *Event) bool
+}
+
+type SamplerFunc func(ctx context.Context, e *Event) bool
+
+func (f SamplerFunc) ShouldSample(ctx context.Context, e *Event) bool {
+	return f(ctx, e)
+}
+
+type DefaultSampler struct {
+	slowThreshold         time.Duration
+	keepHttpStatusAtLeast int
+	randomKeepRate        float64
+}
+
+func NewDefaultSampler() *DefaultSampler {
+	return &DefaultSampler{}
+}
+
+func (s *DefaultSampler) ShouldSample(ctx context.Context, e *Event) bool {
+	if len(e.errors) > 0 {
+		return true
+	}
+
+	if e.duration >= s.slowThreshold {
+		return true
+	}
+
+	httpStatus := 0
+	statusFromMap, exists := e.attrs["request.status"]
+	if exists {
+		success := false
+		httpStatus, success = statusFromMap.(int)
+		if !success {
+			httpStatus = 0
+		}
+	}
+
+	if httpStatus >= s.keepHttpStatusAtLeast {
+		return true
+	}
+
+	if s.randomKeepRate <= rand.Float64() {
+		return true
+	}
+
+	return false
+}
