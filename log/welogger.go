ad00c58f16d6d1c1f85b02062b6f66251460c842
Denis Mishankov <mishankov@mail.com>
2026-02-16 20:30:21 +0300

    Remove default time from wide logs

--- log/welogger.go
+++ log/welogger.go
@@ -14,9 +14,26 @@ type WideEventLogger struct {
 
 // NewWideEventLogger creates a wide-event logger.
 func NewWideEventLogger(w io.Writer, s Sampler, loggerType string, contextKeys map[string]any) *WideEventLogger {
+	opts := &slog.HandlerOptions{
+		Level: slog.LevelDebug,
+		ReplaceAttr: func(_ []string, a slog.Attr) slog.Attr {
+			if a.Key == slog.TimeKey {
+				return slog.Attr{}
+			}
+			return a
+		},
+	}
+
+	var handler slog.Handler
+	if loggerType == "json" {
+		handler = slog.NewJSONHandler(w, opts)
+	} else {
+		handler = slog.NewTextHandler(w, opts)
+	}
+
 	return &WideEventLogger{
 		sampler: s,
-		logger:  New(w, loggerType, slog.LevelDebug, contextKeys),
+		logger:  slog.New(&contextHandler{handler, contextKeys}),
 	}
 }
 
